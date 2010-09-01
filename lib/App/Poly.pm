ad1791e67cceca975032fb7021cc3157bd0ad8de
John SJ Anderson <genehack@genehack.org>
2010-09-01 02:49:11 +0000

    Import of GENEHACK/App-Poly-0.001-TRIAL from CPAN.
    gitpan-cpan-distribution: App-Poly
    gitpan-cpan-version:      0.001-TRIAL
    gitpan-cpan-path:         GENEHACK/App-Poly-0.001-TRIAL.tar.gz
    gitpan-cpan-author:       GENEHACK
    gitpan-cpan-maturity:     developer

---
+++ lib/App/Poly.pm
@@ -0,0 +1,33 @@
+package App::Poly;
+
+# ABSTRACT: A meta-social network interface
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+App::Poly - A meta-social network interface
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+John SJ Anderson <genehack@genehack.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by John SJ Anderson.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
